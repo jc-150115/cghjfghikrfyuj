cbb82f11195f2244ed0c60f2b14912fe0c1474f2
祥平 佐藤 <150115@jc-21.jp>
2017-12-12 14:44:11 +0900

    henko

--- SQLite11/SQLite11/MainPage.xaml.cs
+++ SQLite11/SQLite11/MainPage.xaml.cs
@@ -49,7 +49,9 @@ namespace SQLite11
             var layout = new StackLayout { HorizontalOptions = LayoutOptions.Center, Margin = new Thickness { Top = 100 } };
 
 
-            
+            UserModel.insertUser("鈴木");
+            UserModel.insertUser("田中");
+            UserModel.insertUser("斎藤");
 
 
 
